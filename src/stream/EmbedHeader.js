3ffdcb93465516959216246bf0644c1c059cc37e
Pontus Persson <pom@skurt.com>
2016-12-23 09:00:20 -0800

    Add stream header embed / extract.

--- src/stream/EmbedHeader.js
+++ src/stream/EmbedHeader.js
@@ -2,19 +2,21 @@ const Transform = require('stream').Transform;
 
 class EmbedHeader extends Transform
 {
-    constructor(data) {
+    constructor(data = {}) {
         super({ decodeStrings: false });
         this.setEncoding(null);
 
         this._data = data;
-        this._finished = true;
+        this._finished = false;
     }
 
     _transform(chunk, encoding, done) {
-
-        if (this._finished) {
-            this.push(chunk, encoding);
+        if (!this._finished) {
+            const header = JSON.stringify(this._data);
+            this.push(header + '\n', 'utf8');
+            this._finished = true;
         }
+        this.push(chunk, encoding);
         done();
     }
 }
--- src/stream/ExtractHeader.js
+++ src/stream/ExtractHeader.js
@@ -2,41 +2,32 @@ const Transform = require('stream').Transform;
 
 class ExtractHeader extends Transform
 {
-    constructor(maxLen = 2048) {
+    constructor() {
         super({ decodeStrings: false });
         this.setEncoding(null);
 
-        this._maxLen = maxLen;
         this._buff = '';
-        this._finished = true;
+        this._finished = false;
     }
 
     _transform(chunk, encoding, done) {
         if (this._finished) {
             this.push(chunk, encoding);
         } else {
-            // collect string into buffer
             this._buff += chunk.toString();
 
-            if (this._buff.length > this._maxLen) {
-                throw new Error(`No header found within max length ${this._maxLen}`)
-            }
-
-            // check if string has newline symbol
             const index = this._buff.indexOf('\n');
             if (index !== -1) {
-                const decoded = JSON.parse(this._buff.slice(0, index));
-                this.emit('decoded', decoded);
+                const header = this._buff.slice(0, index);
+                const data = JSON.parse(header);
+                this.emit('decoded', data);
 
-                const data = this._buff.slice(index + 1);
-                const chunk = Buffer.from(data, 'utf8');
-
-                // push to stream skipping first line
-                this.push(chunk);
-                // clear string buffer
-                this._buff = null;
-                // mark as removed
-                this._finished = true;
+                {
+                    const rest = this._buff.slice(index + 1);
+                    this.push(rest, encoding);
+                    this._buff = null;
+                    this._finished = true;
+                }
             }
         }
         done();
