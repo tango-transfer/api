10a4d9468dfa72978007799215b1b54ab00a5c8c
Pontus Persson <pom@skurt.com>
2016-12-24 02:26:41 -0800

    Tweakery.

---
+++ src/public/js/download.js
@@ -0,0 +1,9 @@
+(function() {
+  const element = document.querySelector('.download');
+  const form = element.querySelector('form');
+
+  form.addEventListener('submit', (event) => {
+    element.classList.add('requested');
+    element.querySelector('h1').textContent = 'Hold on';
+  });
+}());
