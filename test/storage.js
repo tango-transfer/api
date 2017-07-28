d868bbe85fadc90113d7529add5d6971a6ef4f46
Pontus Persson <pontus.alexander@gmail.com>
2017-07-28 09:04:42 +0200

    Fix test.

--- test/storage.js
+++ test/storage.js
@@ -1,14 +1,12 @@
 const expect = require('expect.js');
 
 const fs = require('fs');
-const crypto = require('crypto');
 
 const hash = require('../src/hash');
 const Storage = require('../src/Storage');
 
-
 describe('Storage', () => {
-  let storage;
+  let storage, file;
 
   beforeEach(() => {
     storage = new Storage();
@@ -16,57 +14,61 @@ describe('Storage', () => {
   });
 
   context('when storing file to disk', () => {
-    let storePromise;
+    let storePromise, receipt;
 
     beforeEach(() => {
-      const file = fs.createReadStream('./test/fixture/image.png', 'binary');
-      storePromise = storage.store(file, 'image/png', 'other_filename.png');
+      file = fs.createReadStream('./test/fixture/image.png', 'binary');
+      storePromise = storage.store(file, {
+        mime: 'image/png',
+        filename: 'other_filename.png',
+      });
     });
 
-    it('returns a thenable', () => {
-      expect(storePromise.then).to.be.a('function');
+    it('returns a Promise', () => {
+      expect(storePromise).to.be.a(Promise);
     });
 
-    it('resolves new file id', (done) => {
-      storePromise.then(response => {
-        expect(response.receipt.id).to.match(/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/);
-      }).then(done).catch(done);
-    });
+    describe('when resolved', () => {
+      beforeEach(() => {
+        return storePromise.then(response => {receipt = response});
+      });
 
-    it('resolves stream that can be waited for', (done) => {
-      storePromise.then(receipt => {
-        receipt.stream.on('finish', done);
-      }).catch(done);
+      describe('receipt', () => {
+        it('contains file id', () => {
+          expect(receipt.id.length).to.be(8);
+        });
+
+        it('contains secret', () => {
+          expect(receipt.secret.length).to.be(64);
+        });
+      });
     });
 
     context('then retreiving file using receipt id', () => {
       let retreivePromise;
 
-      beforeEach((done) => {
-        storePromise.then(response => {
-          return new Promise(r => {
-            response.stream.on('finish', () => r(response.receipt));
-          });
-        })
-        .then(receipt => {
-          retreivePromise = storage.retrieve(receipt.id);
-        }).then(done).catch(done);
+      beforeEach(() => {
+        retreivePromise = storage.retrieve(receipt.id, receipt.secret);
       });
 
-      it('returns a thenable', () => {
-        expect(retreivePromise.then).to.be.a('function');
+      it('returns a Promise', () => {
+        expect(retreivePromise).to.be.a(Promise);
       });
 
-      it('resolves with meta', (done) => {
-        retreivePromise.then(({meta}) => {
-          expect(meta.contentType).to.be('image/png');
-          expect(meta.name).to.be('other_filename.png');
-        }).then(done).catch(done);
-      });
+      describe('when resolved', () => {
+        let result;
 
-      it('resolves stream with expected data', (done) => {
-        retreivePromise.then((payload) => {
-          hash(payload.stream, 'sha1').then(digest => {
+        beforeEach(() => {
+          return retreivePromise.then(_r => {result = _r});
+        });
+
+        it('contains meta', () => {
+          expect(result.meta.mime).to.be('image/png');
+          expect(result.meta.filename).to.be('other_filename.png');
+        });
+
+        it('contains stream with expected data', (done) => {
+          hash(result.stream, 'sha1').then(digest => {
             expect(digest).to.be('8f4e8178e595b15c062e2d6d1bc9cb25d1101a97');
           }).then(done).catch(done);
         });
